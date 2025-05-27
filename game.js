e3e25d3bc62ca8b6aabdf3634441029c9b71ef3f
rsv13 <rsv130393@gmail.com>
2025-05-27 22:18:46 +0100

    Creating a function to randomly pick the choice of Computer

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+// Writing a function to let computer randomly choose its choice and battle against the human choice
+// Using arrays to store all the available choices and then using Math.random function to pick only one choice at a time
+    function getComputerChoice() {
+        let choice = ["Rock", "Paper", "Scissors"];
+        let randomChoice = Math.floor(Math.random() * choice.length);
+        let compChoice = choice[randomChoice];
+        console.log("Computer chose: " + compChoice.toLowerCase());
+        return compChoice.toLowerCase();
+    }
